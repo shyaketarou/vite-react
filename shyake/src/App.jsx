5f842a6e2a57f0178a3611f46fb088e60f4c1c02
shyaketarou <gzu102997717@gmail.com>
2024-08-07 18:22:40 +0800

    Update App.jsx

--- shyake/src/App.jsx
+++ shyake/src/App.jsx
@@ -1,62 +1,94 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react";
 
+ function InputHello() {
+  const [text, setText] = useState("Please find related articles on");
+  const [inputlinks, setinputlinks] = useState("")
+  const [output, setOutput] = useState("")
+  const [outputlink, setOutputlink] = useState("")
 
-
-function InputHello() {
-  const [text, setText] = useState("Hello world");
 
   function handleChange(event) {
-    setText(event.target.value);
+    setinputlinks(event.target.value);
   }
-
+  function handleClick() {
+    var linkarray= inputlinks.split(/\r?\n/)
+    const mappedData = linkarray.map("link()")
+    console.log(linkarray)
+    
+    
+  }
   return (
     <>
-     
-      <p>{text}</p>
-      <input value={text} onChange={handleChange} />
+      <p>{inputlinks}</p>
+      <textarea value={inputlinks} onChange={handleChange} />
+      <button onClick={handleClick}>submit</button>
+      <p><i>{output} </i><a href={inputlinks}>(link)</a></p>
     </>
   );
 }
 
-function App() {
-  const [a, seta]= useState(0)
-  const [b, setb]= useState(0)
-  const [result, setresult]= useState(null)
-  function handleA(event) {
-    seta(event.target.value);
-  }
-  function handleB(event) {
-    setb(event.target.value);
-  }
-
-
-  function add() {
-    setresult(parseInt(a) + parseInt(b))
-  }
-  function subtract() {
-    setresult(parseInt(a) - parseInt(b))
-  }
-  function multiply() {
-    setresult(parseInt(a) * parseInt(b))
-  }
-  function divide() {
-    setresult(parseInt(a) / parseInt(b))
-  }
-
-  return(
+export default function App() {
+  return (
     <>
-    <h1>ads</h1>
-    <p>{result}</p>
-    <input value={a} onChange={handleA}/>
-    <input value={b} onChange={handleB}/>
-    <button onClick={add}>Add</button>
-    <button onClick={subtract}>subtract</button>
-    <button onClick={multiply}>multiply</button>
-    <button onClick={divide}>divide</button>
+    <InputHello />
+
     </>
   )
 }
 
-export default App
+// function InputHello() {
+//   const [text, setText] = useState("Hello world");
+
+//   function handleChange(event) {
+//     setText(event.target.value);
+//   }
+
+//   return (
+//     <>
+     
+//       <p>{text}</p>
+//       <input value={text} onChange={handleChange} />
+//     </>
+//   );
+// }
+
+// function App() {
+//   const [a, seta]= useState(0)
+//   const [b, setb]= useState(0)
+//   const [result, setresult]= useState(null)
+//   function handleA(event) {
+//     seta(event.target.value);
+//   }
+//   function handleB(event) {
+//     setb(event.target.value);
+//   }
+
+
+//   function add() {
+//     setresult(parseInt(a) + parseInt(b))
+//   }
+//   function subtract() {
+//     setresult(parseInt(a) - parseInt(b))
+//   }
+//   function multiply() {
+//     setresult(parseInt(a) * parseInt(b))
+//   }
+//   function divide() {
+//     setresult(parseInt(a) / parseInt(b))
+//   }
+
+//   return(
+//     <>
+//     <h1>ads</h1>
+//     <p>{result}</p>
+//     <input value={a} onChange={handleA}/>
+//     <input value={b} onChange={handleB}/>
+//     <button onClick={add}>Add</button>
+//     <button onClick={subtract}>subtract</button>
+//     <button onClick={multiply}>multiply</button>
+//     <button onClick={divide}>divide</button>
+//     </>
+//   )
+// }
+
+// export default App
