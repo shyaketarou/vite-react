bb930d0b6723f96ef31f738e21f3cdd24ebb26ea
shyaketarou <gzu102997717@gmail.com>
2024-08-13 11:56:33 +0800

    Final (?)

--- shyake/src/App.jsx
+++ shyake/src/App.jsx
@@ -11,10 +11,11 @@ import { useState } from "react";
 //     'https://www.cdns.com.tw/articles/1061237',
 //     'https://www.chinatimes.com/realtimenews/20240807003746-260407?ctrack=pc_main_rtime_p02&chdtv',
 //     'https://www.ctee.com.tw/news/20240807701190-430101'
+// https://news.cnyes.com/news/id/5681315
 // ]
 
 function convertToName(x) {
-  const arrayLength = x.length //4
+  const arrayLength = x.length 
   const resultArray =[]
   function pushToArray(i, n) {
     if(i==arrayLength-1) {
@@ -35,6 +36,15 @@ function convertToName(x) {
   }
   for (let i=0; i<arrayLength; i++) {
       switch (true) {
+          case x[i].includes("anntw"):
+            pushToArray(i, "Awakening News")
+          break;
+          case x[i].includes("bnext"):
+            pushToArray(i, "BusinssNext")
+          break;
+          case x[i].includes("businesstoday"):
+            pushToArray(i, "Business Today")
+          break;
           case x[i].includes("cna"):
               pushToArray(i, "Central News Agency")
           break;
@@ -46,6 +56,66 @@ function convertToName(x) {
           break;
           case x[i].includes("ctee"):
             pushToArray(i, "Commercial Times")
+          break;
+          case x[i].includes("cnyes"):
+            pushToArray(i, "cnYes")
+          break;
+          case x[i].includes("cts"):
+            pushToArray(i, "CTS News")
+          break;
+          case x[i].includes("digitimes"):
+            pushToArray(i, "DigiTimes")
+          break;
+          case x[i].includes("money.udn"):
+            pushToArray(i, "Economic Daily News")
+          break;
+          case x[i].includes("epochtimes"):
+            pushToArray(i, "Epoch Times")
+          break;
+          case x[i].includes("ettoday"):
+            pushToArray(i, "ETtoday")
+          break;
+          case x[i].includes("ftvnews"):
+            pushToArray(i, "FTV")
+          break;
+          case x[i].includes("hakkanews"):
+            pushToArray(i, "Hakka News")
+          break;
+          case x[i].includes("iknow"):
+            pushToArray(i, "iKnow")
+          break;
+          case x[i].includes("ltn"):
+            pushToArray(i, "Liberty Times")
+          break;
+          case x[i].includes("mirrormedia"):
+            pushToArray(i, "Mirror Media")
+          break;
+          case x[i].includes("moneydj"):
+            pushToArray(i, "MoneyDJ")
+          break;
+          case x[i].includes("newtalk"):
+            pushToArray(i, "Newtalk")
+          break;
+          case x[i].includes("rti"):
+            pushToArray(i, "Radio Taiwan")
+          break;
+          case x[i].includes("taipeitimes"):
+            pushToArray(i, "Taipei Times")
+          break;
+          case x[i].includes("taisounds"):
+            pushToArray(i, "Taisounds")
+          break;
+          case x[i].includes("storm"):
+            pushToArray(i, "The Storm Media")
+          break;
+          case x[i].includes("ttv"):
+            pushToArray(i, "TTV")
+          break;
+          case x[i].includes("tvbs"):
+            pushToArray(i, "TVBS News")
+          break;
+          case x[i].includes("udn"):
+            pushToArray(i, "United Daily News")
           break;
       }
   }
@@ -62,16 +132,17 @@ function convertToName(x) {
   }
   return (
     <>
-      <p>{inputlinks}</p>
+      {/* <p>{inputlinks}</p> */}
       <textarea value={inputlinks} onChange={handleChange} />
       <button onClick={handleClick}>submit</button>
       <p>
       Please find related articles on
       {namedArray.map((link) => (
         <>
-       <i>{link.name}</i> (<a href={link.link}>link</a>) 
-        </>
+       <i>{link.name}</i> (<a href={link.link}><font color="#0070C0">link</font></a>) 
+        </>    
       ))}
+      .
       </p>
     </>
   );
